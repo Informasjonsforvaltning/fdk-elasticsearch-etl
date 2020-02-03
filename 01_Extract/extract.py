a95f3c87a61f1235db3840fbb3950a0c4d587ead
Stig B. Dørmænen <stigbd@gmail.com>
2020-02-03 10:40:31 +0100

    Fix extract - 1st attempt

--- 01_Extract/extract.py
+++ 01_Extract/extract.py
@@ -18,29 +18,26 @@ with open(args.inputfile, encoding='utf-8') as f:
     next(reader, None)
     for row in reader:
         orgNummer = row[0]
-        inputfileName = args.outputdirectory + orgNummer + "_Publisher.json"
-        with open(inputfileName) as json_file:
-            data = json.load('{"query":{"bool":{"must":[{"match":{"publisher.orgPath":"/STAT/972417807/974761076"}}],"must_not":[],"should":[]}},"from":0,"size":10,"sort":[],"aggs":{}}')
-            # PUT THE CORRECT URL IN HERE:
-            host = ''
-            if len(host) == 0:
-                sys.exit('You must provide the url to the server!')
-            url = host + "ccat_v34/_search"
-            headers = {'Content-Type' : 'application/json'}
-            # PUT THE COOKIE NAME:VALUE IN HERE
-            cookieName = 'devshell-proxy-session'
-            cookieValue = ''
-            if len(cookieValue) == 0:
-                sys.exit('You must provide the cookieValue!')
-            cookies={cookieName:cookieValue}
-            print("Posting to the following url: ", url)
-            print("Posting to publisher index the following data:\n", data)
-            # Load the publisher by posting the data:
-            r = requests.post(url, cookies=cookies, json=data, headers=headers)
-            print (orgNummer + ": " + str(r.status_code))
+        data = json.load('{"query":{"bool":{"must":[{"match":{"publisher.orgPath":"/STAT/972417807/974761076"}}],"must_not":[],"should":[]}},"from":0,"size":10,"sort":[],"aggs":{}}')
+        # PUT THE CORRECT URL IN HERE:
+        host = 'https://8888-dot-7801860-dot-devshell.appspot.com/'
+        if len(host) == 0:
+            sys.exit('You must provide the url to the server!')
+        url = host + "ccat_v34/_search"
+        headers = {'Content-Type' : 'application/json'}
+        # PUT THE COOKIE NAME:VALUE IN HERE
+        cookieName = 'devshell-proxy-session'
+        cookieValue = 'b9edd3b588b755a9164d7f396d5f5a17d3962d08a6d005c818ae408eccd5dae8'
+        if len(cookieValue) == 0:
+            sys.exit('You must provide the cookieValue!')
+        cookies={cookieName:cookieValue}
+        print("Posting to the following url: ", url)
+        print("Posting to publisher index the following data:\n", data)
+        # Load the publisher by posting the data:
+        r = requests.post(url, cookies=cookies, json=data, headers=headers)
+        print (orgNummer + ": " + str(r.status_code))
+        with open(args.outputdirectory + orgNummer + '_enhetsregisteret.json', 'w', encoding="utf-8") as outfile:
+            json.dump(r.json(), outfile, ensure_ascii=False, indent=4)
 
-            with open(args.outputdirectory + orgNummer + '_enhetsregisteret.json', 'w', encoding="utf-8") as outfile:
-                json.dump(r.json(), outfile, ensure_ascii=False, indent=4)
-
-                # get total number of rows
-    print("Total no. of organizations from enhetsregisteret: %d"%(reader.line_num - 1))
+       # get total number of rows
+       print("Total no. of organizations from enhetsregisteret: %d"%(reader.line_num - 1))
--- Makefile
+++ Makefile
@@ -3,15 +3,15 @@ all: extract transform load
 
 extract:
 	echo "Extracting...."
-	python3 ./01_extract_organizations/extractOrganizations.py -o ./tmp/ -i ./organizations_15.csv
+	python3 ./01_Extract/extract.py -o ./tmp/ -i ./organizations_01.csv
 
 transform:
 	echo "Transforming...."
-	python3 ./02_transform/transform.py -o ./tmp/ -i ./organizations_15.csv
+	python3 ./02_Transform/transform.py -o ./tmp/ -i ./organizations_01.csv
 
 load:
 	echo "Loading...."
-	python3 ./03_load_publishers/loadPublishers.py -o ./tmp/ -i ./organizations_15.csv
+	python3 ./03_load/load.py -o ./tmp/ -i ./organizations_01.csv
 
 clean:
 	rm ./tmp/*.json
